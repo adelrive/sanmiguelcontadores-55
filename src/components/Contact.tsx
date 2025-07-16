b212763f75999d0b74a0a3bc2226af7f1854649c
Ariel del Rivero <ariel@MacBook-Pro-3.local>
2025-07-16 17:40:02 -0300

    Cambio de prueba simple

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -55,7 +55,7 @@ const Contact = () => {
                 <p className="text-lg font-semibold text-primary mb-2">
                   Av. Belgrano 1217 piso 10, of. 106
                 </p>
-                <p className="text-muted-foreground">(1093) CABA, Argentina</p>
+                <p className="text-muted-foreground">() CABA, Argentina</p>
               </CardContent>
             </Card>
 
